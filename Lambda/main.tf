a522122f0c5b871e51f4455a54bb2c67fd634d0b
sumitmasurkar <sumit.sbm@gmail.com>
2025-03-15 23:29:18 +0530

    Update main.tf

--- Lambda/main.tf
+++ Lambda/main.tf
@@ -6,18 +6,23 @@ provider "aws" {
 data "aws_iam_role" "IAMRoleWithLamdaFullS3Access" {
   role_name = "LamdaFullS3Access"  # Replace with your IAM role name
 }
+data "archive_file" "PythonLambdaPackage" {
+  type = "zip"  
+  source_file = "LambdaFile.py"
+  output_path = "LambdaFile.zip"
+}
 
 # Lambda Function
 resource "aws_lambda_function" "my_lambda" {
   function_name = "MyLambdaFunction"
   role          = data.aws_iam_role.existing_lambda_role.arn  # Use the ARN of the existing role
   handler       = "index.handler"  # Your Lambda function entry point (adjust as needed)
-  runtime       = "python3.9"     # Update this to the appropriate runtime (e.g., python3.8, etc.)
+  runtime       = "python3.8"     # Update this to the appropriate runtime (e.g., python3.8, etc.)
   timeout       = 300               # Timeout in seconds
   memory_size   = 128              # Memory allocated for the Lambda function
 
   # Path to your Lambda function deployment package
-  filename = "lambda_function_payload.zip"  # Path to your Lambda deployment package
+  filename = "LambdaFile.zip"  # Path to your Lambda deployment package
 
   environment {
     variables = {
